4b094ffa9a4cf2bd05ffcbc544d97f20dc009da8
hautari <103041382+hautari@users.noreply.github.com>
2022-06-16 10:08:36 +0300

    deleted extra files
    deleted extra files

--- src/main/java/com/mulesoft/connectors/baswareconnectoroauth2aprilv2/internal/config/base/BaswareConnectorOauth2aprilv2ConfigurationBase.java
+++
@@ -1,5 +0,0 @@
-package com.mulesoft.connectors.baswareconnectoroauth2aprilv2.internal.config.base;
-
-import com.mulesoft.connectivity.rest.commons.api.configuration.RestConfiguration;
-
-public abstract class BaswareConnectorOauth2aprilv2ConfigurationBase extends RestConfiguration {}
