7a0eb92e6215f93d87b98f692be08766a8d42a43
hautari <103041382+hautari@users.noreply.github.com>
2022-08-10 10:45:08 +0300

    latest updates

--- src/main/java/com/mulesoft/connectors/baswareconnector/internal/config/base/BaswareConfigurationBase.java
+++
@@ -1,5 +0,0 @@
-package com.mulesoft.connectors.baswareconnector.internal.config.base;
-
-import com.mulesoft.connectivity.rest.commons.api.configuration.RestConfiguration;
-
-public abstract class BaswareConfigurationBase extends RestConfiguration {}
--- src/main/java/com/mulesoft/connectors/baswareconnector/internal/connection/provider/refinement/Oauth2ConnectionProviderRefinement.java
+++
@@ -1,9 +0,0 @@
-package com.mulesoft.connectors.baswareconnector.internal.connection.provider.refinement;
-
-import com.mulesoft.connectors.baswareconnector.internal.connection.provider.base.Oauth2ConnectionProviderBase;
-
-/**
- * This Refinement class is the middle layer of the connection provider generation gap pattern. It
- * can be overridden to add custom logic in the connection provider.
- */
-public class Oauth2ConnectionProviderRefinement extends Oauth2ConnectionProviderBase {}
